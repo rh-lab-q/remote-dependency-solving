f7449b665e66c3b469316ca5f856a3abaa5ba77f
Michal Ruprich <Formaiko@Formaiko-PC.nat7.vcit.vutbr.net>
2014-10-07 15:50:05 +0200

    added flush_xml into xml_handler2.hpp for better debugging

--- src/common/xml_handler2.hpp
+++ src/common/xml_handler2.hpp
@@ -4,12 +4,65 @@
 #include <libxml/parser.h>
 #include <libxml/xmlmemory.h>
 #include <libxml/xpath.h>
+#include <libxml/xmlwriter.h>
 #include <iostream>
 #include <string>
 #include <vector>
 
 namespace ssds_xml
 {
+	class xml_debug{
+	public:
+		xml_debug()
+		{
+			
+		}
+	
+		void flush_xml(xmlNode * a_node, int indent)
+		{
+			xmlNode *curr_node = NULL;
+			std::string tab = "";
+			
+			for(int i=0; i<indent; i++) {
+				tab += "\t";
+			}
+
+			for (curr_node = a_node; curr_node; curr_node = curr_node->next) {
+				if(curr_node->type == 1)//classic node - just print the begginning of its name
+					std::cout << tab << "<" << curr_node->name;
+				if(curr_node->type != 3){//if it has no attributes - ending >
+					if(curr_node->properties == nullptr)
+						std::cout << ">";
+					else{//otherwise print all attributes
+						xmlAttrPtr attribute = curr_node->properties;
+						while(attribute!=nullptr){
+							std::cout << " " << attribute->name << "=\"";//name of attribute
+							std::cout << attribute->children->content << "\"";//value of the attribute
+							
+							attribute = attribute->next;
+						}
+						std::cout << ">";//ending with attributes
+					}
+				}
+				
+				
+				
+				if(curr_node->content != nullptr)
+					std::cout << curr_node->content;
+				
+				flush_xml(curr_node->children, indent+1);
+				
+				if(curr_node->type == 1) {
+						std::cout<<tab;//the tab will be there even when it is not supposed to... but nevermind 
+						std::cout  << "</" << curr_node->name << ">";
+				}
+			}
+			
+			
+			//std::cout << tab << "</" << curr_node->name << ">";
+		}
+	};
+	
 	//object representing xml attribute 
 	class xml_attr{
 		public:
@@ -48,6 +101,9 @@ namespace ssds_xml
 				
 				rootNodePtr = xmlDocGetRootElement(document);
 				currNodePtr = rootNodePtr;
+				
+				ssds_xml::xml_debug debug;
+				debug.flush_xml(rootNodePtr, 0);
 
 				while(currNodePtr != NULL)
 				{
@@ -84,9 +140,19 @@ namespace ssds_xml
 			}
 			
 			/*
-			 *TODO - tady to udelam asi tak ze se vrati int a zustane ukazatel kam ma, zbytek se udela primo v main 
+			 * Function finds all nodes with specified name and returns its contents in a structure reachable through ret_vector_ptr
+			 * Structure referenced by ret_vector_ptr needs to be dynamically allocated before calling this func
+			 * After calling this func, it is very simple to go through the structure:
+			 * 
+			 * for(std::vector<ssds_xml::xml_node*>::iterator it = result->begin(); it != result->end(); it++){//result is of the same type as ret_vector_ptr
+			 *		std::cout << (*it)->value << std::endl; //this prints the value of the node i.e. any text between <tag> and </tag>
+			 *		
+			 *		for(std::vector<ssds_xml::xml_attr*>::iterator itt = (*it)->attributes.begin(); itt != (*it)->attributes.end(); itt++){
+			 *			std::cout << "\tattr_name: " << (*itt)->name << ", attr_value:" << (*itt)->value << std::endl;//this prints attributes values and names
+			 *		}
+			 *	}
 			 */
-			void find_node_by_path(xmlChar* path, std::vector<xml_node*>* ret_vector_ptr)
+			void get_node_by_path(xmlChar* path, std::vector<xml_node*>* ret_vector_ptr)
 			{
 				xmlXPathContextPtr context;
 				xmlXPathObjectPtr result;
@@ -133,6 +199,38 @@ namespace ssds_xml
 				}*/
 			}
 			
+			xmlNodePtr add_node_by_path(xmlChar* xpath)
+			{
+				xmlXPathContextPtr context;
+				xmlXPathObjectPtr result;
+							
+				context = xmlXPathNewContext(document);
+				result = xmlXPathEvalExpression(xpath, context);
+								
+				if(xmlXPathNodeSetIsEmpty(result->nodesetval)){
+					xmlXPathFreeObject(result);
+					std::cout << "nenaslo" << std::endl;
+					return nullptr;
+				}
+							
+				if(result->nodesetval->nodeNr > 1){
+					std::cout << "ambiguous xpath expression" << std::endl;
+					return nullptr;
+				}
+									
+									
+				std::cout << "probehlo" << std::endl;
+							
+				//std::cout << result->nodesetval->nodeTab[0]->name << std::endl;
+									
+									
+									
+			}
+			
+			
+			
+			
+			
 		public:
 			xmlDocPtr document;
 			xmlNodePtr currNodePtr;
@@ -142,19 +240,55 @@ namespace ssds_xml
 	};
 	
 	
+	
+	
+	
 	class create_xml
 	{
 		
 	public:
 		create_xml()
 		{
+			/*std::string contents;
+			writer = xmlNewTextWriterFilename("text_output.xml", 0);
 			
+			xmlTextWriterStartDocument(writer, NULL, "UTF-8", NULL);
+			xmlTextWriterStartElement(writer,BAD_CAST "ssds");
+			xmlTextWriterWriteElement(writer,BAD_CAST  "code",BAD_CAST  "123");
+			xmlTextWriterEndElement(writer);
+			
+			xmlTextWriterWriteElement(writer,BAD_CAST  "data",BAD_CAST  "ahoj");
+			xmlTextWriterEndElement(writer);
+			
+			
+			xmlTextWriterEndElement(writer);
+			xmlTextWriterEndDocument(writer);*/
+			
+			std::string base = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><ssds><code></code><data></data></ssds>";
+			document = xmlParseMemory(base.c_str(), base.size());
+			
+			rootNodePtr = xmlDocGetRootElement(document);
+			currNodePtr = rootNodePtr;
+			
+			ssds_xml::xml_debug debug;
+			//debug.flush_xml(rootNodePtr, 0);
 		}	
 		
+		xmlNodePtr create_node_by_path(xmlChar* xpath)
+		{
+			
+						
+						
+		}
+		
 	public:
+		xmlTextWriterPtr writer;
 		xmlDocPtr document;
 		xmlNodePtr rootNodePtr;
+		xmlNodePtr currNodePtr;
 	};
+	
+	
 
 }
 
