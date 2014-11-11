a7307a508ed93e6393f736e20b1026e38c5229a4
Michal Ruprich <Formaiko@Formaiko-PC.nat7.vcit.vutbr.net>
2014-11-11 13:06:53 +0100

    return_code.hpp with basic return codes Fixes #8

---
+++ src/common/return_code.hpp
@@ -0,0 +1,22 @@
+#ifndef RETURN_CODE_HPP
+#define RETURN_CODE_HPP
+
+/*
+ * Message codes for finite automata on either side of communication
+ */
+#define BASIC_INSTALL 0
+
+
+/*
+ * Error codes for returning as status messages from either side of communication
+ * These are reserved for SSDS error codes not for internal server errors
+ */
+#define UNKNOWN_COMMAND 100
+#define UNKNOWN_REPO 101
+#define UNKNOWN_PKG 102
+
+/*
+ * Internal server error codes
+ */
+
+#endif
