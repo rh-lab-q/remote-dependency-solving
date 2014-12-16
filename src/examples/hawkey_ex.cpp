e8fe3f5b821ead8fac45acf2ca5a26431efcc243
Michal Ruprich <Formaiko@Formaiko-PC.nat7.vcit.vutbr.net>
2014-12-16 11:38:26 +0100

    hawkey example - right now I was just testing including various libraries

---
+++ src/examples/hawkey_ex.cpp
@@ -0,0 +1,12 @@
+#include "../common/hawkey_handler.hpp"
+#include <iostream>
+//#include "../../../hawkey/hawkey/src/sack.h"
+#include <solv/testcase.h>
+
+using namespace std;
+
+int main(){
+  
+  cout << "Hello World!!!" << endl;
+  return 1;
+}
