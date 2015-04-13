e7c165727e5e2018d9357dee6a65b04712b2016b
Jozkar <J.Ridky@gmail.com>
2015-04-13 12:01:37 +0200

    Std c++11 error

--- libhif-test/test.cpp
+++ libhif-test/test.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-#include <libhif/libhif.h>
+//#include <hawkey/types.h>
+//#include <libhif/libhif.h>
+//#include <libhif/hif-package.h>
 int main(){
 
-	struct HifState *state;
-	struct HyPackage *pkg;
-	hif_package_set_filename(&pkg, "testovaci nazev");
-	const gchar *directory = "/home/jozkar/Plocha/download/";
-	GError **error;	
+//	HifState state;
+//	HyPackage pkg;
+//	hif_package_set_filename(pkg, "testovaci nazev");
+//	const gchar *directory = "/home/jozkar/Plocha/download/";
+//	GError **error;	
 	
 	std::cout << "Testovaci priklad" << std::endl;
 	
