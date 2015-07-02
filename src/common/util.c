1f0823df94b858c010870138da346620aef4676f
walter <walter@d02-0202b.kn.vutbr.cz>
2015-07-02 15:06:09 +0200

    Util module created
    Utility module created, currently implements resolving path to @System.solv file which depends on system version.

---
+++ src/common/util.c
@@ -0,0 +1,56 @@
+/* Server side dependency solving - transfer of dependency solving from local machine to server when installing new packages
+ * Copyright (C) 2015  Michal Ruprich, Josef Řídký, Walter Scherfel
+ *
+ * Licensed under the GNU Lesser General Public License Version 2.1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "util.h"
+
+char * ssds_resolve_dependency_file_path()
+{
+    system("uname -r > version.tmp"); //printing system info to file
+
+    FILE * f = fopen("version.tmp","r");
+    int c, i = 0;
+    char fedora_version[10]; //22 or 21 or any other
+    char system_version[7]; //64bit(x86_64) or 32bit(i686)
+    char path[40];
+
+    while((c = fgetc(f)) != 'c'); //skipping to fedora version
+
+    //reading fedora version
+    while(isdigit(c = fgetc(f)))
+    {
+        fedora_version[i] = c;
+        i++;
+    }
+    fedora_version[i] = '\0';
+    i = 0;
+
+    //reading system version
+    while(isalnum(c = fgetc(f)) || (c == '_'))
+    {
+        system_version[i] = c;
+        i++;
+    }
+    system_version[i] = '\0';
+
+    //composing path to @System.solv file
+    snprintf(path, 40, "/var/cache/dnf/%s/%s/@System.solv",system_version, fedora_version);
+    fclose(f);
+    system("rm version.tmp"); //removing temporary file
+}
---
+++ src/common/util.h
@@ -0,0 +1,44 @@
+/* Server side dependency solving - transfer of dependency solving from local machine to server when installing new packages
+ * Copyright (C) 2015  Michal Ruprich, Josef Řídký, Walter Scherfel
+ *
+ * Licensed under the GNU Lesser General Public License Version 2.1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef _UTIL_H
+#define _UTIL_H
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * Resolves path for @System.solv file depending on system version
+ * @brief ssds_resolve_dependency_file_path
+ * @return path to @System.solv file
+ */
+char * ssds_resolve_dependency_file_path();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
