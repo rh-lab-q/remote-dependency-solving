ba50afd79d81a90043c268b8ba507ded5076f16d
Jozkar <J.Ridky@gmail.com>
2015-09-03 09:26:32 +0200

    Removed useless parts of server source code

--- src/server/server_main.c
+++ src/server/server_main.c
@@ -21,28 +21,6 @@
 #include "server.h"
 
 
-#if 0
-void session(ip::tcp::socket sock,boost::system::error_code ec){
-	try {
-		while(42)
-		{
-			char data[1024];
-			size_t lengthsome = sock.read_some(boost::asio::buffer(data), ec);
-			 if (ec == boost::asio::error::eof) {
-				 std::cout << "Connection closed." << "\n";
-		 	 	 break; // Connection closed cleanly by peer.
-			 }
-			  else if (ec)
-			      throw boost::system::system_error(ec);
-		      	  boost::asio::write(sock, boost::asio::buffer(data, lengthsome));
-		 }//while
-		}//try
-	 catch (std::exception& e) {
-		    std::cerr << "Exception in thread: " << e.what() << "\n";
-	 }
-}
-#endif
-
 int main(int argc, char* argv[]) {
   /*******************************************************************/
   /* Setting up garbage collector and setting callback functions */
@@ -340,53 +318,7 @@ int core()
     ssds_close(comm_sock);
     ssds_close(data_sock);
   }
-  //ssds_solving::solve solveHandler;
 
-  
-  
-//   HySack* sack = ssds_solve_init();
-//   ssds_fill_sack(sack, list);
-
-#if 0
-  try {
-    ip::tcp::iostream streams("");
-    while(42) {
-      ip::tcp::socket sock(ios);
-      acceptor_.accept(sock); //second argument can be error handler
-
-      std::cout<<"Some connection was accepted" << std::endl;
-
-      mainserver.process_connection(sock);
-#if 0
-      int64_t size;
-      boost::asio::read(sock,boost::asio::buffer(&size,sizeof(size)));
-
-      std::vector<char> buf(size);
-      size_t len = sock.read_some(buffer(buf), ec);
-      std::string input_message = std::string(buf.begin(), buf.end());
-
-      std::cout<< "Message has " << len << " characters." << std::endl;
-
-      solving
-      std::string message = solvePoint.answer(input_message);
-#endif	
-
-      sock.close();
-
-      //far future		    
-      //std::thread(session, std::move(sock).std::move(ec)).detach();
-
-    }
-  } /*handling exceptions*/
-  catch (std::exception& e){
-    //std::ostringstream os;
-    //os << "Server: "<< e.what();
-    /*std::cerr*///my_log.add_log(logERROR,e.what());
-    ssds_log("pokus", logERROR);
-    //ssds_log(e.what(), logERROR); //might not work since e.what returns virtual const char*
-  }
-
-#endif
   ssds_gc_cleanup();
   return EXIT_SUCCESS;
 }
