7e2b2b519fa97bd1d3c8f9915004125115ef7565
SimonMatej <nomeesh@outlook.cz>
2015-11-25 14:49:01 +0100

    removed delay from network_security
    sleep() was called for debug purposes, it could cause unwanted delay

--- src/common/network_security.c
+++ src/common/network_security.c
@@ -31,16 +31,16 @@ SSL *ssl;						// SSL connection
 
 ssize_t ssds_read(int fd, void *buf, size_t count)
 {
-	sleep(1); ssds_log(logDEBUG, "Reading max %d bytes of data\n", count);
+	ssds_log(logDEBUG, "Reading max %d bytes of data\n", count);
 	int res = -1;
 	if (_use_secure_connection == 0)
 	{
-		sleep(1); ssds_log(logDEBUG, "Using nonciphered read\n");
+		ssds_log(logDEBUG, "Using nonciphered read\n");
 		res = read(fd, buf, count);
 	}
 	else
 	{
-		sleep(1); ssds_log(logDEBUG, "Using ciphered read\n");
+		ssds_log(logDEBUG, "Using ciphered read\n");
 		if (_ssl_connected == 0) // initialize connection
 		{
 			if (initialize_secure_connection(fd) == -1)
@@ -50,34 +50,34 @@ ssize_t ssds_read(int fd, void *buf, size_t count)
 			}
 			else
 			{
-				sleep(1); ssds_log(logMESSAGE, "SSL successfully connected\n");
+				ssds_log(logMESSAGE, "SSL successfully connected\n");
 				_ssl_connected = 1;
 			}
 		}
 		res = SSL_read(ssl, buf, count);
 		if (res == -1)
 		{
-			sleep(1); ssds_log(logERROR, "SSL read failed\n");
+			ssds_log(logERROR, "SSL read failed\n");
 			ERR_print_errors_fp(stderr);
 		}
 	}
-	sleep(1); ssds_log(logDEBUG, "Read %d bytes\n", res);
+	ssds_log(logDEBUG, "Read %d bytes\n", res);
 	return res;
 }
 
 
 ssize_t ssds_write(int fd, const void *buf, size_t count)
 {
-	sleep(1); ssds_log(logDEBUG, "Writing %d bytes of data\n", count);
+	ssds_log(logDEBUG, "Writing %d bytes of data\n", count);
 	int res = -1;
 	if (_use_secure_connection == 0)
 	{
-		sleep(1); ssds_log(logDEBUG, "Using nonciphered write\n");
+		ssds_log(logDEBUG, "Using nonciphered write\n");
 		res = write(fd, buf, count);
 	}
 	else
 	{
-		sleep(1); ssds_log(logDEBUG, "Using ciphered write\n");
+		ssds_log(logDEBUG, "Using ciphered write\n");
 		if (_ssl_connected == 0) // not connected
 		{
 			if (initialize_secure_connection(fd) == -1)
@@ -87,41 +87,41 @@ ssize_t ssds_write(int fd, const void *buf, size_t count)
 			}
 			else
 			{
-				sleep(1); ssds_log(logMESSAGE, "SSL successfully connected\n");
+				ssds_log(logMESSAGE, "SSL successfully connected\n");
 				_ssl_connected = 1;
 			}
 		}
 		res = SSL_write(ssl, buf, count);
 		if (res == -1)
 		{
-			sleep(1); ssds_log(logERROR, "SSL write failed\n");
+			ssds_log(logERROR, "SSL write failed\n");
 			ERR_print_errors_fp(stderr);
 		}
 	}
-	sleep(1); ssds_log(logDEBUG, "Written %d bytes\n", res);
+	ssds_log(logDEBUG, "Written %d bytes\n", res);
 	return res;
 }
 
 
 int initialize_secure_connection(int fd)
 {
-	sleep(1); ssds_log(logDEBUG, "Init SSL\n");
+	ssds_log(logDEBUG, "Init SSL\n");
 	sleep(5);
 	ctx = InitCTX();
 
 	//server loads certificate and key
 	#ifdef __CASE_SERVER
-	sleep(1); ssds_log(logDEBUG, "Server is loading certificates\n");
+	ssds_log(logDEBUG, "Server is loading certificates\n");
 	if (LoadCertificates(ctx, MY_CERT, MY_KEY) == -1)
 	{
-		sleep(1); ssds_log(logERROR, "SSL fail (server load certs), using non-ciphered connection\n");
+		ssds_log(logERROR, "SSL fail (server load certs), using non-ciphered connection\n");
 		//ERR_print_errors_fp(stderr);
 		_use_secure_connection = 0;
 		return -1;
 	}
 	else
 	{
-		sleep(1); ssds_log(logMESSAGE, "SSL Certificates Successfully loaded\n");
+		ssds_log(logMESSAGE, "SSL Certificates Successfully loaded\n");
 	}
 	#endif
 
@@ -132,14 +132,14 @@ int initialize_secure_connection(int fd)
 	#ifdef __CASE_CLIENT
 	if (SSL_connect(ssl) == -1)
 	{
-		sleep(1); ssds_log(logERROR, "SSL fail (client connect), using non-ciphered connection\n");
+		ssds_log(logERROR, "SSL fail (client connect), using non-ciphered connection\n");
 		ERR_print_errors_fp(stderr);
 		_use_secure_connection = 0;
 		return -1;
 	}
 	else
 	{
-		sleep(1); ssds_log(logMESSAGE, "Successfully connected\n");
+		ssds_log(logMESSAGE, "Successfully connected\n");
 	}
 	#endif
 
@@ -147,20 +147,20 @@ int initialize_secure_connection(int fd)
 	#ifdef __CASE_SERVER
 	if (SSL_accept(ssl) == -1)
 	{
-		sleep(1); ssds_log(logERROR, "SSL fail (server accept), using non-ciphered connection\n");
+		ssds_log(logERROR, "SSL fail (server accept), using non-ciphered connection\n");
 		ERR_print_errors_fp(stderr);
 		_use_secure_connection = 0;
 		return -1;
 	}
 	else
 	{
-		sleep(1); ssds_log(logMESSAGE, "Successfully accepted\n");
+		ssds_log(logMESSAGE, "Successfully accepted\n");
 	}
 	#endif
 
 	if (ShowCerts(ssl) == -1)
 	{
-		sleep(1); ssds_log(logERROR, "SSL fail (no peer certificates avalaible)\n");
+		ssds_log(logERROR, "SSL fail (no peer certificates avalaible)\n");
 		ERR_print_errors_fp(stderr);
 		//_use_secure_connection = 0;
 		//return -1;
@@ -180,7 +180,7 @@ SSL_CTX* InitCTX(void)
 	ctx = SSL_CTX_new(method);
 	if (SSL_CTX_set_cipher_list(ctx, CIPHERS) != 1)
 	{
-		sleep(1); ssds_log(logERROR, "Couldnt use any ciphers, switching to non-ciphered connection\n");
+		ssds_log(logERROR, "Couldnt use any ciphers, switching to non-ciphered connection\n");
 		_use_secure_connection = 0;
 	}
 	if ( ctx == NULL )
@@ -198,18 +198,18 @@ int ShowCerts(SSL* ssl)
 	cert = SSL_get_peer_certificate(ssl);
 	if ( cert != NULL )
 	{
-		sleep(1); ssds_log(logDEBUG, "Server certificates:\n");
+		ssds_log(logDEBUG, "Server certificates:\n");
 		line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
-		sleep(1); ssds_log(logDEBUG, "Subject: %s\n", line);
+		ssds_log(logDEBUG, "Subject: %s\n", line);
 		free(line);
 		line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
-		sleep(1); ssds_log(logDEBUG, "Issuer: %s\n", line);
+		ssds_log(logDEBUG, "Issuer: %s\n", line);
 		free(line);
 		X509_free(cert);
 		return 0;
 	}
 	else
-		sleep(1); ssds_log(logERROR, "No peer certificates avalaible.\n");
+		ssds_log(logERROR, "No peer certificates avalaible.\n");
 	return -1;
 }
 
@@ -219,18 +219,18 @@ int LoadCertificates(SSL_CTX* ctx, char* CertFile, char* KeyFile)
     if (SSL_CTX_use_certificate_file(ctx, CertFile, SSL_FILETYPE_PEM) <= 0)
     {
         //ERR_print_errors_fp(stderr);
-        sleep(1); ssds_log(logERROR, "Couldn load certificate\n");
+        ssds_log(logERROR, "Couldn load certificate\n");
         return -1;
     }
     if (SSL_CTX_use_PrivateKey_file(ctx, KeyFile, SSL_FILETYPE_PEM) <= 0)
     {
         //ERR_print_errors_fp(stderr);
-        sleep(1); ssds_log(logERROR, "Couldnt load private key\n");
+        ssds_log(logERROR, "Couldnt load private key\n");
         return -1;
     }
     if (!SSL_CTX_check_private_key(ctx))
     {
-        sleep(1); ssds_log(logERROR, "\
+        ssds_log(logERROR, "\
         	Private key does not match the certificate public key\n");
         return -1;
     }
